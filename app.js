2153c93e65132c1cd447103f08645f26b5301318
roytwQQ <roysashimi@gmail.com>
2014-09-25 02:38:58 +0800

    done the core functionality

--- app.js
+++ app.js
@@ -5,14 +5,23 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+/*----------- START router area -------------*/
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var photos = require('./routes/photos');
+/*----------- END router area -------------*/
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+//the dir that for img upload
+app.set('photos', path.join(__dirname, 'public/photos')); 
+
+//for router/photo.js get dir
+global.photosDir = app.get('photos');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -22,8 +31,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+/*------------ START url to router------------------*/
+app.use('/', photos);
+app.use('/upload', photos);
 app.use('/users', users);
+/*------------ END url to router------------------*/
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/photos.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+
+var Photo = require('../models/Photo');
+var path = require('path');
+var fs = require('fs');
+var join = path.join;
+
+//form express4, upload multipart(img), use bellow
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+
+
+router.get('/', function(req, res, next) {
+	Photo.find({}, function(err, photos){
+		if (err) return next(err);
+		res.render('photos', {
+		title: 'Photos',
+		photos: photos
+		});
+		});
+
+});
+
+
+router.get('/upload', function(req, res) {
+	res.render('upload', {
+		title: 'Photo upload'
+	});
+
+
+});
+
+
+router.post('/upload', multipartMiddleware, function(req, res, next) {
+var img = req.files.photo.image;
+var name = req.body.photo.name || img.name;
+var path = join(global.photosDir, img.name);
+fs.rename(img.path, path, function(err){
+if (err) return next(err);
+Photo.create({
+name: name,
+path: img.name
+}, function (err) {
+if (err) return next(err);
+res.redirect('/');
+});
+});
+
+});
+
+module.exports = router;
