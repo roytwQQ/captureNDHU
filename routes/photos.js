f20b3ec0f9963df7af83e3fd5e989e6a56b335c8
roytwQQ <roysashimi@gmail.com>
2014-11-21 02:33:36 +0800

    fixed io

--- routes/photos.js
+++ routes/photos.js
@@ -36,16 +36,31 @@ router.post('/upload', multipartMiddleware, function(req, res, next) {
 var img = req.files.photo.image;
 var name = req.body.photo.name || img.name;
 var path = join(global.photosDir, img.name);
-fs.rename(img.path, path, function(err){
-if (err) return next(err);
-Photo.create({
-name: name,
-path: img.name
-}, function (err) {
-if (err) return next(err);
-res.redirect('/photos');
+
+var is = fs.createReadStream(img.path);
+var os = fs.createWriteStream(path);
+
+is.pipe(os);
+is.on('end',function() {
+    fs.unlinkSync(img.path);
+    Photo.create({
+	name: name,
+	path: img.name
+	}, function (err) {
+	if (err) return next(err);
+	res.redirect('/photos');
+	});
 });
-});
+// fs.rename(img.path, path, function(err){
+// if (err) return next(err);
+// Photo.create({
+// name: name,
+// path: img.name
+// }, function (err) {
+// if (err) return next(err);
+// res.redirect('/photos');
+// });
+// });
 
 });
 
