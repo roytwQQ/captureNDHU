170897bbc5e68591c68e9eb24ff7e77a72d7d7df
roytwQQ <roysashimi@gmail.com>
2014-11-21 01:06:07 +0800

    test

--- models/Photo.js
+++ models/Photo.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/photo_app');
+// mongoose.connect('mongodb://localhost/photo_app');
+mongoose.connect('mongodb://fuckyou:shit@dogen.mongohq.com:10002/app31802368');
 
 var schema = new mongoose.Schema({
 	name: String,
