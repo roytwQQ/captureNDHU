fe31323d64782b6bb69fb95e1989c67875775182
roytwQQ <roysashimi@gmail.com>
2015-01-08 16:04:50 +0800

    gogo

--- app.js
+++ app.js
@@ -24,12 +24,13 @@ app.set('photos', path.join(__dirname, 'public/photos'));
 global.photosDir = app.get('photos');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/NDHU.ico'));
 
 /*------------ START url to router------------------*/
 app.use('/photos', photos);
